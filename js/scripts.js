45d365ecd60ce7820dae3d6fc607afa84a2c5ae6
MaritzaVst <maritzasintafur@gmail.com>
2017-01-18 12:32:48 -0500

    Funcionalidad 2: al dar click en add card se añade textarea

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,21 @@ $(document).ready(function(){
 	$(".save").click(function(e){
 		e.preventDefault();
 		var titleCard = add.val();
-		if (titleCard != ""){
-			$(".wrapper").prepend("<div class='list-wrapper card-list'><div class='list-content'></div></div>");
+		if (titleCard !== ""){
+			$(".wrapper-content").prepend("<div class='list-wrapper card-list'><div class='list-content'></div></div>");
 			$(".list-content").html(titleCard);
+			$("<div class='add-card-content'><a href='#'>Añadir tarjeta...</a></div>").insertAfter(".list-content");
 			$(add).val("");
 		}
+		addCard();
 	});
+	function addCard () {
+		var anchor = $(".add-card-content").find("a");
+		anchor.click(function(e){
+			e.preventDefault();
+			anchor.addClass("hidden");
+			$(".add-card-content").prepend("<textarea></textarea><button class='btn btn-default save'>Guardar</button><span class='glyphicon glyphicon-remove' aria-hidden='true'></span>");
+		});
+	}
+	
 });
